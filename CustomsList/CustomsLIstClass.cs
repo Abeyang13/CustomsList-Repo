c492174bd4ba8b644bdee1fa76acd3ee92a2ed1a
ABRAHAM YANG <abeyang13@gmail.com>
2019-08-13 14:40:05 -0500

    Got done making an array expansion unit test and made a get at index unit test.

---
+++ CustomsList/CustomsLIstClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomsList
+{
+    public class CustomsListClass<T>
+    {
+        private T[] items;
+
+
+
+
+        public CustomsListClass()
+        {
+            items = new T[5];
+        }
+
+        public void Add(T itemToAdd)
+        {
+            items[] = items;
+        }
+
+    }
+}
---
+++ CustomsListTest/CustomsTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomsList;
+
+namespace CustomsListTest
+{
+    [TestClass]
+    public class CustomsTest
+    {
+        [TestMethod]
+        public void Add_AddToEmptyList_ItemGoesToIndexZero()
+        {
+            // arrange
+            CustomsListClass<int> testList = new CustomsListClass<int>();
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(1);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddToListWithExistingItem_NewItemGoesToEndOfList()
+        {
+            // arrange
+            CustomsListClass<int> testList = new CustomsListClass<int>();
+            int expected = 2;
+            int actual;
+
+            // act
+            testList.Add(1);
+            testList.Add(2);
+            actual = testList[1];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddToEmptyList_CountGoesToOne()
+        {
+            // arrange
+            CustomsListClass<int> testList = new CustomsListClass<int>();
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(5);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        // write a test for when the array size changes (what is you array's starting size?)
+        [TestMethod]
+        public void ArraySize_StartingSize_ArrayExpansion()
+        {
+            // arrange
+            CustomsListClass<int> testList = new CustomsListClass<int>();
+            testList = [4];
+
+
+            int actual;
+
+            // act
+            testList.Add(5);
+            testList.Add(6);
+            testList.Add(7);
+            testList.Add(8);
+            testList.Add(9);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(testList, actual);
+        }
+    }
+}
